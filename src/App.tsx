dab018e1bf38e98503ced6f839a3bb95f411359d
Johannes Brodwall <johannes@brodwall.com>
2024-01-04 22:56:01 +0100

    Show a map with Openlayers

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {MutableRefObject, useEffect, useRef} from "react";
+import {Map, View} from "ol";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+import {useGeographic} from "ol/proj";
+
+useGeographic();
+const map = new Map({
+  view: new View({ center: [11, 60], zoom: 10, constrainResolution: true }),
+  layers: [
+      new TileLayer({source: new OSM()})
+  ]
+})
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div ref={mapRef} style={{width: "100vw", height: "100vh"}}></div>
   )
 }
 
