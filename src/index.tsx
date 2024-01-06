033d852c43148ca6c84b40333883429652ce01cb
Johannes Brodwall <johannes@brodwall.com>
2024-01-06 21:19:56 +0100

    npm i -D @types/react @types/react-dom && update tsconfig.json for react

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById("app")!);
 root.render(<h1>Hello world</h1>);
