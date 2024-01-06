9b95d06fb65cf589a18d43eac9bf5138f0494bdf
Johannes Brodwall <johannes@brodwall.com>
2024-01-06 22:33:08 +0100

    log kommunenavn on hover

--- src/modules/map/toggleKommuneCheckbox.tsx
+++ src/modules/map/toggleKommuneCheckbox.tsx
@@ -3,25 +3,51 @@ import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { MapContext } from "./mapContext";
+import { MapBrowserEvent, MapEvent } from "ol";
+
+interface KommuneProperties {
+  kommunenummer: string;
+  navn: { sprak: string; navn: string }[];
+}
 
 export function ToggleKommuneCheckbox() {
   const [showKommuner, setShowKommuner] = useState(false);
-  const kommuneLayer = useMemo(
-    () =>
-      new VectorLayer({
-        source: new VectorSource({
-          url: "/kommuner.json",
-          format: new GeoJSON(),
-        }),
+  const [kommuneAtMouse, setKommuneAtMouse] = useState<
+    KommuneProperties | undefined
+  >(undefined);
+  const kommuneLayer = useMemo(() => {
+    return new VectorLayer({
+      source: new VectorSource({
+        url: "/kommuner.json",
+        format: new GeoJSON(),
       }),
-    [],
-  );
-  const { setLayers } = useContext(MapContext);
+    });
+  }, []);
+  function handlePointerMove(e: MapBrowserEvent<MouseEvent>) {
+    const featuresAtCoordinate = kommuneLayer
+      .getSource()
+      ?.getFeaturesAtCoordinate(e.coordinate);
+    const kommune = (
+      featuresAtCoordinate
+        ? featuresAtCoordinate[0]?.getProperties()
+        : undefined
+    ) as KommuneProperties | undefined;
+    setKommuneAtMouse((old) =>
+      old?.kommunenummer != kommune?.kommunenummer ? kommune : old,
+    );
+  }
+
+  useEffect(() => {
+    console.log(kommuneAtMouse?.navn.find((n) => n.sprak === "nor")?.navn);
+  }, [kommuneAtMouse]);
+  const { setLayers, map } = useContext(MapContext);
   useEffect(() => {
     if (showKommuner) {
       setLayers((old) => [...old, kommuneLayer]);
+      map.on("pointermove", handlePointerMove);
     } else {
       setLayers((old) => old.filter((l) => l != kommuneLayer));
+      map.un("pointermove", handlePointerMove);
     }
   }, [showKommuner]);
 
