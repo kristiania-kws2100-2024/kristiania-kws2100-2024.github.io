6bf002e1b6ac29a8a5b4b7bcbe9d4b64650bfdda
Johannes Brodwall <johannes@brodwall.com>
2024-01-06 21:59:50 +0100

    zoom around in the map

--- src/modules/map/mapNav.tsx
+++ src/modules/map/mapNav.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { ReactNode, useContext } from "react";
+import { MapContext } from "./mapContext";
+
+function LinkButton({
+  onClick,
+  children,
+}: {
+  onClick(): void;
+  children: ReactNode;
+}) {
+  function handleClick(e: React.MouseEvent) {
+    e.preventDefault();
+    onClick();
+  }
+  return (
+    <a href={"#"} onClick={handleClick}>
+      {children}
+    </a>
+  );
+}
 
 export function MapNav() {
+  const { view } = useContext(MapContext);
+  function handleShowNorway() {
+    view.animate({ center: [16, 65], zoom: 5 });
+  }
+  function handleShowOslo() {
+    view.animate({ center: [10.5, 59.8], zoom: 10 });
+  }
+  function handleZoomToUser() {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      view.animate({ center: [longitude, latitude], zoom: 14 });
+    });
+  }
+
   return (
     <nav>
-      <div>Do something</div>
+      <LinkButton onClick={handleShowNorway}>Show Norway</LinkButton>
+      <LinkButton onClick={handleShowOslo}>Show Oslo</LinkButton>
+      <LinkButton onClick={handleZoomToUser}>Show my location</LinkButton>
     </nav>
   );
 }
