5b324d64d2a6bf1b72c1cf6899c54c2747c80f67
Johannes Brodwall <johannes@brodwall.com>
2024-01-06 21:18:05 +0100

    npm i -D prettier tsc && npx tsc --init

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(<h1>Hello world</h1>)
+root.render(<h1>Hello world</h1>);
