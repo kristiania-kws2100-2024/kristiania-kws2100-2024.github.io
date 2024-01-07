81e920c02c0402bf7a42da3f7d26436a60315928
Johannes Brodwall <johannes@brodwall.com>
2024-01-07 17:57:33 +0100

    Create menu for application

--- src/modules/application/application.tsx
+++ src/modules/application/application.tsx
@@ -3,10 +3,19 @@ import "./application.css";
 import "ol/ol.css";
 import { MapContextProvider } from "../map/mapContextProvider";
 import { MapView } from "../map/mapView";
+import { KommuneLayerCheckbox } from "../kommune/kommuneLayerCheckbox";
 
 export function Application() {
   return (
     <MapContextProvider>
+      <header>
+        <h1>Kommunekart</h1>
+      </header>
+      <nav>
+        <a href={"#"}>Focus on me</a>
+        <a href={"#"}>Show Norway</a>
+        <KommuneLayerCheckbox />
+      </nav>
       <MapView />
     </MapContextProvider>
   );
---
+++ src/modules/kommune/kommuneLayerCheckbox.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function KommuneLayerCheckbox() {
+  return (
+    <div>
+      <label>
+        <input type="checkbox" />
+        Show kommuner
+      </label>
+    </div>
+  );
+}
