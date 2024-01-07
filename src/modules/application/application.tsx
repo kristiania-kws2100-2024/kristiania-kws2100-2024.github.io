77e418912e19dd90ca022667d3560bbef8c4f3fe
Johannes Brodwall <johannes@brodwall.com>
2024-01-07 17:48:22 +0100

    Display minimal map

--- src/modules/application/application.tsx
+++ src/modules/application/application.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 import "./application.css";
 
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+
+useGeographic();
+
+const map = new Map({
+  view: new View({ center: [10.5, 60], zoom: 10 }),
+  layers: [new TileLayer({ source: new OSM() })],
+});
+
 export function Application() {
-  return <main>I'm a map</main>;
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  useEffect(() => map.setTarget(mapRef.current), []);
+  return <main ref={mapRef}></main>;
 }
