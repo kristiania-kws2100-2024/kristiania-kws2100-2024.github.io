678b93e1df15a33d8af12436704a02bc834b331a
Johannes Brodwall <johannes@brodwall.com>
2024-01-07 18:37:45 +0100

    kommuneLayerCheckbox.tsx just barely working

--- src/modules/kommune/kommuneLayerCheckbox.tsx
+++ src/modules/kommune/kommuneLayerCheckbox.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+import { MapContext } from "../map/mapContextProvider";
 
 export function KommuneLayerCheckbox() {
+  const [show, setShow] = useState(false);
+  const { setLayers } = useContext(MapContext);
+
+  const kommuneLayer = useMemo(() => {
+    return new VectorLayer({
+      source: new VectorSource({
+        url: "/kommuner.json",
+        format: new GeoJSON(),
+      }),
+    });
+  }, []);
+
+  useEffect(() => {
+    if (show) {
+      setLayers((old) => [...old, kommuneLayer]);
+    }
+  }, [show]);
+
   return (
     <div>
       <label>
-        <input type="checkbox" />
-        Show kommuner
+        <input
+          type="checkbox"
+          checked={show}
+          onChange={(e) => setShow(e.target.checked)}
+        />
+        {show ? "Hide" : "Show"} kommuner
       </label>
     </div>
   );
--- src/modules/map/mapContextProvider.tsx
+++ src/modules/map/mapContextProvider.tsx
@@ -1,16 +1,38 @@
-import React, { ReactNode } from "react";
+import React, {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
+import { Layer } from "ol/layer";
 
 useGeographic();
-const map = new Map({
-  view: new View({ center: [10.5, 60], zoom: 10 }),
-  layers: [new TileLayer({ source: new OSM() })],
-});
-export const MapContext = React.createContext({ map });
+
+const view = new View({ center: [10.5, 60], zoom: 10 });
+const layers = [new TileLayer({ source: new OSM() })];
+const map = new Map({ view, layers });
+
+const setLayers: Dispatch<SetStateAction<Layer[]>> = () => {};
+
+export const MapContext = React.createContext({ map, setLayers });
 
 export function MapContextProvider({ children }: { children: ReactNode }) {
-  return <MapContext.Provider value={{ map }}>{children}</MapContext.Provider>;
+  const [layers, setLayers] = useState<Layer[]>(() => [
+    new TileLayer({ source: new OSM() }),
+  ]);
+  const map = useMemo(() => {
+    return new Map({ view: new View({ center: [10.5, 60], zoom: 10 }) });
+  }, []);
+  useEffect(() => map.setLayers(layers), [layers]);
+  return (
+    <MapContext.Provider value={{ map, setLayers }}>
+      {children}
+    </MapContext.Provider>
+  );
 }
