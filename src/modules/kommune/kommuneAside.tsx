de3dda8bd2735fa12ed23cbc413469a1dccfef87
Johannes Brodwall <johannes@brodwall.com>
2024-01-07 21:49:57 +0100

    Style feature in map when user hovers in KommuneAside

--- src/modules/kommune/kommuneAside.tsx
+++ src/modules/kommune/kommuneAside.tsx
@@ -2,6 +2,8 @@ import React, { useMemo } from "react";
 import { getKommuneName, KommuneProperties } from "./kommuneProperties";
 import { useViewExtent } from "../map/useViewExtent";
 import { useFeatureLayer } from "../map/useFeatureLayer";
+import { Feature } from "ol";
+import { Stroke, Style, Text } from "ol/style";
 
 export function KommuneAside() {
   const { features, layer } = useFeatureLayer(
@@ -10,17 +12,39 @@ export function KommuneAside() {
   const viewExtent = useViewExtent();
 
   const kommuneList = useMemo(() => {
-    return features.filter(
-      (f) => f.getGeometry()?.intersectsExtent(viewExtent),
-    );
+    return features
+      .filter((f) => f.getGeometry()?.intersectsExtent(viewExtent))
+      .sort((a, b) =>
+        getKommuneName(a.getProperties() as KommuneProperties).localeCompare(
+          getKommuneName(b.getProperties() as KommuneProperties),
+        ),
+      );
   }, [features, viewExtent]);
+
+  function handleMouseEnter(feature?: Feature) {
+    for (const f of features) {
+      f.setStyle(
+        f === feature
+          ? new Style({
+              stroke: new Stroke({ color: "red" }),
+              text: new Text({
+                text: getKommuneName(f.getProperties() as KommuneProperties),
+              }),
+            })
+          : undefined,
+      );
+    }
+  }
 
   return (
     <aside className={layer ? "visible" : ""}>
-      <div>
+      <div onMouseLeave={() => handleMouseEnter(undefined)}>
         <h2>Kommuner</h2>
         {kommuneList.map((k) => (
-          <div key={k.getProperties().kommunenummer}>
+          <div
+            key={k.getProperties().kommunenummer}
+            onMouseEnter={() => handleMouseEnter(k)}
+          >
             {getKommuneName(k.getProperties() as KommuneProperties)}
           </div>
         ))}
