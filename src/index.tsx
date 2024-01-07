d590f44ba1d147fcbe96d792432316412062dfba
Johannes Brodwall <johannes@brodwall.com>
2024-01-07 17:36:17 +0100

    11. `npx prettier --write .`

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello React</h1>)
+root.render(<h1>Hello React</h1>);
