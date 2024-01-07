e647b7cb0445a1aab862c9c843d3534188508983
Johannes Brodwall <johannes@brodwall.com>
2024-01-07 12:50:28 +0100

    KommuneAside shows list of kommune on map (update with view)
    * Limitation: Doesn't update with loading

--- src/modules/kommune/kommuneAside.tsx
+++ src/modules/kommune/kommuneAside.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { MapContext } from "../map/mapContext";
 import VectorSource from "ol/source/Vector";
 import { KommuneProperties } from "./kommune";
@@ -9,21 +9,27 @@ export function KommuneAside() {
     return layers.find((l) => l.getClassName() === "kommuner");
   }, [layers]);
 
+  const [viewExtent, setViewExtent] = useState(
+    () => view.getViewStateAndExtent().extent,
+  );
+  function handleViewChange() {
+    setViewExtent(view.getViewStateAndExtent().extent);
+  }
+  useEffect(() => {
+    view.on("change", handleViewChange);
+    return () => view.un("change", handleViewChange);
+  }, []);
+
   const kommuneList = useMemo(() => {
     return kommuneLayer
       ? (kommuneLayer.getSource() as VectorSource)
           .getFeatures()
-          .filter(
-            (f) =>
-              f
-                .getGeometry()
-                ?.intersectsExtent(view.getViewStateAndExtent().extent),
-          )
+          .filter((f) => f.getGeometry()?.intersectsExtent(viewExtent))
           .map((f) => f.getProperties() as KommuneProperties)
           .map((k) => k.navn.find((n) => n.sprak === "nor")!.navn)
           .sort()
       : [];
-  }, [kommuneLayer]);
+  }, [kommuneLayer, viewExtent]);
 
   return (
     <aside className={kommuneLayer ? "is-open" : ""}>
