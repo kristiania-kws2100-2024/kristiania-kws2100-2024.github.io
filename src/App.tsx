1ab3b9011f28925b6a251fd581a32d7a224cbc4f
Johannes Brodwall <johannes@brodwall.com>
2024-01-10 14:24:11 +0100

    Some cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
+import "ol/ol.css";
+
 import {Map, View} from "ol";
-import {MutableRefObject, useEffect, useRef} from "react";
+import {MutableRefObject, useEffect, useMemo, useRef} from "react";
 import TileLayer from "ol/layer/Tile";
 import {OSM} from "ol/source";
 import {useGeographic} from "ol/proj";
@@ -8,18 +10,19 @@ import {useGeographic} from "ol/proj";
 
 useGeographic();
 
-const map = new Map({
-  layers: [
-      new TileLayer({source: new OSM()})
-  ],
-  view: new View({
-    center: [10.5, 59.7], zoom: 10
-  })
-});
-
 
 
 function App() {
+  const map = useMemo(() => new Map({
+    layers: [
+      new TileLayer({source: new OSM()})
+    ],
+    view: new View({
+      center: [10.5, 59.7], zoom: 11
+    })
+  }), []);
+
+
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
 
   useEffect(() => {
@@ -27,7 +30,7 @@ function App() {
   }, []);
 
   return (
-    <div className={"map"} ref={mapRef}>I am a map</div>
+    <div className={"map"} ref={mapRef}></div>
   )
 }
 
