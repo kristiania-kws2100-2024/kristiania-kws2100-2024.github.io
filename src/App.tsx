486c8880515428f5d4379daf8c4469f85a8c0fe2
Johannes Brodwall <johannes@brodwall.com>
2024-01-10 14:39:30 +0100

    Zoom map in and out

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,33 @@ useGeographic();
 
 
 function App() {
+  const view = useMemo(() => new View({
+    center: [10.5, 59.7], zoom: 11
+  }), []);
   const map = useMemo(() => new Map({
     layers: [
       new TileLayer({source: new OSM()})
     ],
-    view: new View({
-      center: [10.5, 59.7], zoom: 11
-    })
+    view: view
   }), []);
 
+  function handleZoomToUser(e: React.MouseEvent) {
+    e.preventDefault();
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { longitude, latitude } = pos.coords;
+      const center = [longitude, latitude];
+      map.getView().animate({ center, zoom: 14 });
+    })
+  }
+
+  function handleZoomToNorway(e: React.MouseEvent) {
+    e.preventDefault();
+
+    map.getView().animate({
+      center: [15, 65], zoom: 5
+    })
+  }
 
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
 
@@ -30,7 +48,14 @@ function App() {
   }, []);
 
   return (
+      <>
+      <header><h1>Kristiania mapping application</h1></header>
+        <nav>
+          <a href={"#"} onClick={handleZoomToUser}>Zoom to my location</a>
+          <a href={"#"} onClick={handleZoomToNorway}>Show all of Norway</a>
+        </nav>
     <div className={"map"} ref={mapRef}></div>
+      </>
   )
 }
 
