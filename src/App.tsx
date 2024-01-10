97226b7cabf87fd5b856011da9de84768f52a0cc
Johannes Brodwall <johannes@brodwall.com>
2024-01-10 14:00:29 +0100

    Initial openlayers map

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import './App.css'
+import {Map, View} from "ol";
+import {MutableRefObject, useEffect, useRef} from "react";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+import {useGeographic} from "ol/proj";
+
+
+useGeographic();
+
+const map = new Map({
+  layers: [
+      new TileLayer({source: new OSM()})
+  ],
+  view: new View({
+    center: [10.5, 59.7], zoom: 10
+  })
+});
+
+
 
 function App() {
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    map.setTarget(mapRef.current)
+  }, []);
+
   return (
-    <div className={"map"}>I am a map</div>
+    <div className={"map"} ref={mapRef}>I am a map</div>
   )
 }
 
