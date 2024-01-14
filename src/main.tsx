e2291363c1dc3e22fca5d29826b1e70b18acc3df
Johannes Brodwall <johannes@brodwall.com>
2024-01-14 13:16:46 +0100

    fix prettier formatting

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
+<<<<<<< HEAD
+root.render(<h1>Hello React</h1>);
+=======
 root.render(<h1>Hello React</h1>)
+>>>>>>> origin/reference/02
