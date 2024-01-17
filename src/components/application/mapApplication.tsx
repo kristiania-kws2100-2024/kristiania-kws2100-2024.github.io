29330805ed182aea6e0a4a09900e45e6551d9482
Johannes Brodwall <johannes@brodwall.com>
2024-01-17 14:57:47 +0100

    show o

--- src/components/application/mapApplication.tsx
+++ src/components/application/mapApplication.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {MutableRefObject, useEffect, useRef} from "react";
 
 import "./application.css";
+import {Map, View} from "ol";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+import {useGeographic} from "ol/proj";
+
+useGeographic()
+
+const map = new Map({
+    layers: [
+        new TileLayer({
+            source: new OSM()
+        })
+    ],
+    view: new View({
+        center: [10, 59], zoom: 8
+    })
+});
 
 
 export function MapApplication() {
+    const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+    useEffect(() => {
+        map.setTarget(mapRef.current);
+    }, []);
+
     return <>
         <header><h1>Map Application</h1></header>
         <nav>Actions</nav>
-        <main>Here is the map</main>
+        <main ref={mapRef}>Here is the map</main>
     </>;
 }
