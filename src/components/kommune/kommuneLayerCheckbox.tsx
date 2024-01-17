7659309ef2aa96bc5ffd259325dfd579e17b3f39
Johannes Brodwall <johannes@brodwall.com>
2024-01-17 15:44:45 +0100

    now toggling kommuner works

--- src/components/kommune/kommuneLayerCheckbox.tsx
+++ src/components/kommune/kommuneLayerCheckbox.tsx
@@ -27,7 +27,11 @@ export function KommuneLayerCheckbox({
   );
   const [checked, setChecked] = useState(false);
   useEffect(() => {
-    setLayers((old) => [...old, kommuneLayer]);
+    if (checked) {
+      setLayers((old) => [...old, kommuneLayer]);
+    } else {
+      setLayers((old) => old.filter((l) => l !== kommuneLayer));
+    }
   }, [checked]);
 
   return (
