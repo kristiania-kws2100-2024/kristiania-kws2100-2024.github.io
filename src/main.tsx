efc078e99ff5b583ac933139ae63d00280e52858
Johannes Brodwall <johannes@brodwall.com>
2024-01-21 18:37:18 +0100

    run prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {MapApplication} from "./modules/application/mapApplication";
+import { MapApplication } from "./modules/application/mapApplication";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
-root.render(<MapApplication/>);
+root.render(<MapApplication />);
