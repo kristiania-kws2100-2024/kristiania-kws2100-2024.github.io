a8208c30198cbf0806cfac82dd9a5ef058b05d5b
Johannes Brodwall <johannes@brodwall.com>
2024-01-21 18:09:10 +0100

    Add OpenLayers with a simple map

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {MapApplication} from "./modules/application/mapApplication";
+
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>Hello React</h1>);
+
+root.render(<MapApplication/>);
