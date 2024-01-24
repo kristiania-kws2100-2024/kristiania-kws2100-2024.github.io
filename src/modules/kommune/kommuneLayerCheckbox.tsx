6a2c12e21722939b84ecf49ad24fdc20167c6e13
Johannes Brodwall <johannes@brodwall.com>
2024-01-24 13:49:20 +0100

    Showing kommune navn in overlay

--- src/modules/kommune/kommuneLayerCheckbox.tsx
+++ src/modules/kommune/kommuneLayerCheckbox.tsx
@@ -1,9 +1,17 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import React, {
+  Dispatch,
+  MutableRefObject,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Layer } from "ol/layer";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
-import { Feature, Map, MapBrowserEvent } from "ol";
+import { Feature, Map, MapBrowserEvent, Overlay } from "ol";
 import { Polygon } from "ol/geom";
 
 type KommuneProperties = {
@@ -34,13 +42,28 @@ export function KommuneLayerCheckbox({
   setLayers: Dispatch<SetStateAction<Layer[]>>;
 }) {
   const [checked, setChecked] = useState(false);
+  const overlay = useMemo(() => new Overlay({}), []);
+  const overlayRef = useRef() as MutableRefObject<HTMLDivElement>;
+  useEffect(() => {
+    overlay.setElement(overlayRef.current);
+    map.addOverlay(overlay);
+    return () => {
+      map.removeOverlay(overlay);
+    };
+  }, []);
+  const [selectedKommune, setSelectedKommune] = useState<
+    KommuneFeature | undefined
+  >();
   function handleClick(e: MapBrowserEvent<MouseEvent>) {
     const clickedKommune = kommuneSource.getFeaturesAtCoordinate(
       e.coordinate,
     ) as KommuneFeature[];
     if (clickedKommune.length === 1) {
-      const properties = clickedKommune[0].getProperties() as KommuneProperties;
-      alert(properties.navn.find((n) => n.sprak === "nor")!.navn);
+      setSelectedKommune(clickedKommune[0]);
+      overlay.setPosition(e.coordinate);
+    } else {
+      setSelectedKommune(undefined);
+      overlay.setPosition(undefined);
     }
   }
 
@@ -65,6 +88,17 @@ export function KommuneLayerCheckbox({
         />
         {checked ? "Hide" : "Show"} kommune layer
       </label>
+      <div ref={overlayRef}>
+        {selectedKommune && (
+          <>
+            {
+              (selectedKommune.getProperties() as KommuneProperties).navn.find(
+                (n) => n.sprak === "nor",
+              )!.navn
+            }
+          </>
+        )}
+      </div>
     </div>
   );
 }
