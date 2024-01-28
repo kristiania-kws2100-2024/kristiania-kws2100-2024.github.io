544bcee6491f2658b76203827779dcb02d8c3538
Johannes Brodwall <johannes@brodwall.com>
2024-01-28 12:12:57 +0100

    Toggle showing kommune layer

--- src/modules/kommune/kommuneLayerCheckbox.tsx
+++ src/modules/kommune/kommuneLayerCheckbox.tsx
@@ -1,7 +1,29 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { MapContext } from "../map/mapContext";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+
+const kommuneLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/kommuner.json",
+    format: new GeoJSON(),
+  }),
+});
 
 export function KommuneLayerCheckbox() {
+  const { setLayers } = useContext(MapContext);
   const [checked, setChecked] = useState(true);
+
+  useEffect(() => {
+    if (checked) {
+      setLayers((old) => [...old, kommuneLayer]);
+    }
+    return () => {
+      setLayers((old) => old.filter((l) => l !== kommuneLayer));
+    };
+  }, [checked]);
+
   return (
     <div>
       <label>
