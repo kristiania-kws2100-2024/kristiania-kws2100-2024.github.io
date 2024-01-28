23c8bd2c740b86b9cc9b214815f90f1be36d69a6
Johannes Brodwall <johannes@brodwall.com>
2024-01-28 12:47:52 +0100

    Filter features in KommuneAside based on visible extent (unfinished)

--- src/modules/kommune/kommuneAside.tsx
+++ src/modules/kommune/kommuneAside.tsx
@@ -4,12 +4,21 @@ import { MapContext } from "../map/mapContext";
 import { KommuneLayer, KommuneFeature } from "./kommuneLayer";
 
 export function KommuneAside() {
-  const { layers } = useContext(MapContext);
+  const { layers, map } = useContext(MapContext);
+  const viewExtent = useMemo(
+    () => map.getView().getViewStateAndExtent().extent,
+    [map],
+  );
   const kommuneLayer = useMemo(
     () => layers.find((l) => l.getClassName() === "kommuner") as KommuneLayer,
     [layers],
   );
   const [kommuner, setKommuner] = useState<KommuneFeature[]>([]);
+  const visibleFeatures = useMemo(() => {
+    return kommuner.filter((f) =>
+      f.getGeometry()?.intersectsExtent(viewExtent),
+    );
+  }, [kommuner, viewExtent]);
   function loadKommuneFeatures() {
     setKommuner(kommuneLayer?.getSource()?.getFeatures() || []);
   }
@@ -25,8 +34,8 @@ export function KommuneAside() {
   return (
     <aside className={kommuneLayer ? "show" : "hide"}>
       <div>
-        <h2>{kommuner.length} Kommuner</h2>
-        {kommuner.map((k) => (
+        <h2>{visibleFeatures.length} Kommuner</h2>
+        {visibleFeatures.map((k) => (
           <div key={k.getProperties().kommunenummer}>
             {k.getProperties().navn.find((n) => n.sprak === "nor")!.navn}
           </div>
