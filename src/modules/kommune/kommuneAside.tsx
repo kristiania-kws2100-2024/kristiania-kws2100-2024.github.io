41400cd749220745cb18358c54214b1c054b8739
Johannes Brodwall <johannes@brodwall.com>
2024-01-31 12:57:22 +0100

    Show and hide all kommune features in the KommuneAside

--- src/modules/kommune/kommuneAside.tsx
+++ src/modules/kommune/kommuneAside.tsx
@@ -1,15 +1,21 @@
 import React, { useContext } from "react";
 import { MapContext } from "../map/mapContext";
+import VectorSource from "ol/source/Vector";
 
 export function KommuneAside() {
   const { layers } = useContext(MapContext);
 
   const kommuneLayer = layers.find((l) => l.getClassName() === "kommuner");
+  const features = (kommuneLayer?.getSource() as VectorSource)?.getFeatures();
 
   return (
     <aside className={kommuneLayer ? "visible" : "hidden"}>
       <div>
         <h2>Kommuner</h2>
+
+        <ul>
+          {features?.map((k) => <li>{k.getProperties().kommunenummer}</li>)}
+        </ul>
       </div>
     </aside>
   );
