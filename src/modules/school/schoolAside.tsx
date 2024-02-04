7b3030763c392106a8d0740991afe408f6b5af09
Johannes Brodwall <johannes@brodwall.com>
2024-02-04 22:44:47 +0100

    show active school in schoolAside.tsx

--- src/modules/school/schoolAside.tsx
+++ src/modules/school/schoolAside.tsx
@@ -1,23 +1,29 @@
 import { useVectorFeatures } from "../map/useVectorFeatures";
-import React from "react";
+import React, { useState } from "react";
 import { SchoolFeature } from "./schoolFeature";
 
 export function SchoolAside() {
   const { visibleFeatures } = useVectorFeatures<SchoolFeature>(
     (l) => l.getClassName() === "schools",
   );
+  const [activeFeature, setActiveFeature] = useState<
+    SchoolFeature | undefined
+  >();
 
   return (
     <aside className={visibleFeatures.length ? "visible" : "hidden"}>
       <div>
         <h2>Skoler</h2>
-        <ul>
+        <ul onMouseLeave={() => setActiveFeature(undefined)}>
           {[...visibleFeatures]
             .sort((a, b) =>
               a.getProperties().navn.localeCompare(b.getProperties().navn),
             )
             .map((s) => (
-              <li>
+              <li
+                onMouseEnter={() => setActiveFeature(s)}
+                className={activeFeature === s ? "active" : ""}
+              >
                 {s.getProperties().navn} ({s.getProperties().antall_elever}{" "}
                 elever)
               </li>
