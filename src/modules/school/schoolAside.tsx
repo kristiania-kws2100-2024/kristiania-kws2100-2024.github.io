810eae05679e8b26b1a9dbd3aecb069251a7a43e
Johannes Brodwall <johannes@brodwall.com>
2024-02-04 22:29:34 +0100

    Show school properties in SchoolAside

--- src/modules/school/schoolAside.tsx
+++ src/modules/school/schoolAside.tsx
@@ -1,8 +1,22 @@
 import { useVectorFeatures } from "../map/useVectorFeatures";
 import React from "react";
+import { Feature } from "ol";
+import { Point } from "ol/geom";
+
+type SchoolFeature = { getProperties(): SchoolProperties } & Feature<Point>;
+
+interface SchoolProperties {
+  navn: string;
+  antall_elever: number;
+  antall_ansatte: number;
+  laveste_trinn: number;
+  hoyeste_trinn: number;
+  eierforhold: "Offentlig" | "Privat";
+  kommunenummer: string;
+}
 
 export function SchoolAside() {
-  const { visibleFeatures } = useVectorFeatures(
+  const { visibleFeatures } = useVectorFeatures<SchoolFeature>(
     (l) => l.getClassName() === "schools",
   );
 
@@ -10,7 +24,9 @@ export function SchoolAside() {
     <aside className={visibleFeatures?.length ? "visible" : "hidden"}>
       <div>
         <h2>Skoler</h2>
-        <ul>{visibleFeatures?.map((k) => <li>school</li>)}</ul>
+        <ul>
+          {visibleFeatures?.map((s) => <li>{s.getProperties().navn}</li>)}
+        </ul>
       </div>
     </aside>
   );
