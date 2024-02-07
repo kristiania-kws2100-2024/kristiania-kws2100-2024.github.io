e9163476234026fbea16cdd8383ab3f5aefe1cfe
Johannes Brodwall <johannes@brodwall.com>
2024-02-07 12:40:02 +0100

    show schools on map

--- src/modules/school/schoolLayerCheckbox.tsx
+++ src/modules/school/schoolLayerCheckbox.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLayer } from "../map/useLayer";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+
+const schoolLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/schools.json",
+    format: new GeoJSON(),
+  }),
+});
 
 export function SchoolLayerCheckbox() {
+  const [checked, setChecked] = useState(true);
+
+  useLayer(schoolLayer, checked);
+
   return (
     <div>
       <label>
-        <input type={"checkbox"} />
+        <input
+          type={"checkbox"}
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
         Show schools
       </label>
     </div>
