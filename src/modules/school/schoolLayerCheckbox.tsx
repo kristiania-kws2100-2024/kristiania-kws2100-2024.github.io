8d08acec260470edd4df23752232d8d8986b7d34
Johannes Brodwall <johannes@brodwall.com>
2024-02-07 12:43:58 +0100

    custom style of schoole

--- src/modules/school/schoolLayerCheckbox.tsx
+++ src/modules/school/schoolLayerCheckbox.tsx
@@ -3,11 +3,19 @@ import { useLayer } from "../map/useLayer";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
+import { Circle, Fill, Stroke, Style } from "ol/style";
 
 const schoolLayer = new VectorLayer({
   source: new VectorSource({
     url: "/schools.json",
     format: new GeoJSON(),
+  }),
+  style: new Style({
+    image: new Circle({
+      stroke: new Stroke({ color: "white", width: 2 }),
+      fill: new Fill({ color: "blue" }),
+      radius: 4,
+    }),
   }),
 });
 
