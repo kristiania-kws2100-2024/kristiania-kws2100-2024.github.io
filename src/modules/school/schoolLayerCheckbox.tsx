1beaf55fa464c328ee9ec64a0c297e26643193f2
Johannes Brodwall <johannes@brodwall.com>
2024-02-07 12:52:44 +0100

    move schoolFeature to top level

--- src/modules/school/schoolLayerCheckbox.tsx
+++ src/modules/school/schoolLayerCheckbox.tsx
@@ -4,21 +4,34 @@ import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { Circle, Fill, Stroke, Style } from "ol/style";
+import { Feature } from "ol";
+import { Point } from "ol/geom";
+import { FeatureLike } from "ol/Feature";
 
 const schoolLayer = new VectorLayer({
   source: new VectorSource({
     url: "/schools.json",
     format: new GeoJSON(),
   }),
-  style: (feature) =>
-    new Style({
-      image: new Circle({
-        stroke: new Stroke({ color: "white", width: 2 }),
-        fill: new Fill({ color: "blue" }),
-        radius: 3 + feature.getProperties().antall_elever / 150,
-      }),
-    }),
+  style: schoolStyle,
 });
+
+interface SchoolProperties {
+  antall_elever: number;
+}
+
+type SchoolFeature = { getProperties(): SchoolProperties } & Feature<Point>;
+
+function schoolStyle(f: FeatureLike) {
+  const feature = f as SchoolFeature;
+  return new Style({
+    image: new Circle({
+      stroke: new Stroke({ color: "white", width: 2 }),
+      fill: new Fill({ color: "blue" }),
+      radius: 3 + feature.getProperties().antall_elever / 150,
+    }),
+  });
+}
 
 export function SchoolLayerCheckbox() {
   const [checked, setChecked] = useState(true);
