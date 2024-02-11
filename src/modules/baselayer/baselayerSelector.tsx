d4101e9c8f79dfe8b361e0d0e3edee8c680c9a3d
Johannes Brodwall <johannes@brodwall.com>
2024-02-11 14:53:40 +0100

    include stadia maps as layer options

--- src/modules/baselayer/baselayerSelector.tsx
+++ src/modules/baselayer/baselayerSelector.tsx
@@ -1,12 +1,40 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import TileLayer from "ol/layer/Tile";
+import { OSM, StadiaMaps } from "ol/source";
+import { MapContext } from "../map/mapContext";
 
 export function BaselayerSelector() {
-  const [selected, setSelected] = useState<string>("osm");
+  const { setBaseLayer } = useContext(MapContext);
+  const options = {
+    osm: {
+      name: "Open Street Map",
+      layer: new TileLayer({ source: new OSM() }),
+    },
+    stadia: {
+      name: "Stadia",
+      layer: new TileLayer({ source: new StadiaMaps({ layer: "outdoors" }) }),
+    },
+    dark: {
+      name: "Stadia (dark)",
+      layer: new TileLayer({
+        source: new StadiaMaps({ layer: "alidade_smooth_dark" }),
+      }),
+    },
+  };
+  const [selected, setSelected] = useState<keyof typeof options>("osm");
+
+  useEffect(() => {
+    if (options[selected]) setBaseLayer(options[selected].layer);
+  }, [selected]);
+
   return (
     <div>
       <label>
         Basiskart ({selected}):
-        <select onChange={(e) => setSelected(e.target.value)} value={selected}>
+        <select
+          onChange={(e) => setSelected(e.target.value as any)}
+          value={selected}
+        >
           <option value={"osm"}>Open Street Map</option>
           <option value={"stadia"}>Stadia</option>
           <option value={"dark"}>Stadia (dark)</option>
