27b5ab092303b57f30c809acacf8cd2e56bb29ca
Johannes Brodwall <johannes@brodwall.com>
2024-02-11 14:36:19 +0100

    Define base layers

--- src/modules/baselayer/baselayerSelector.tsx
+++ src/modules/baselayer/baselayerSelector.tsx
@@ -1,3 +1,20 @@
+import React, { useState } from "react";
+
 export function BaselayerSelector() {
-  return null;
+  const [selected, setSelected] = useState<string>("osm");
+  return (
+    <div>
+      <label>
+        Basiskart ({selected}):
+        <select onChange={(e) => setSelected(e.target.value)} value={selected}>
+          <option value={"osm"}>Open Street Map</option>
+          <option value={"stadia"}>Stadia</option>
+          <option value={"dark"}>Stadia (dark)</option>
+          <option value={"kartverket"}>Kartverket (WFS)</option>
+          <option value={"bilder"}>Norge i bilder (WFS)</option>
+          <option value={"polar"}>Polar</option>
+        </select>
+      </label>
+    </div>
+  );
 }
