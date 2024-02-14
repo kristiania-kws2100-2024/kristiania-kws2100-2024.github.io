b24e0b7fd1154a9d93e47d4b8285a3dac3dcbe50
Johannes Brodwall <johannes@brodwall.com>
2024-02-14 12:47:51 +0100

    Working selector (but of course it doesn't change the map)

--- src/modules/baseLayer/baseLayerDropdown.tsx
+++ src/modules/baseLayer/baseLayerDropdown.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function BaseLayerDropdown() {
+  const baseLayerOptions = [
+    {
+      id: "osm",
+      name: "Open Street Map",
+    },
+    {
+      id: "stadia",
+      name: "Stadia",
+    },
+  ];
+  const [selectedLayer, setSelectedLayer] = useState(baseLayerOptions[0]);
+
   return (
-    <select>
-      <option>First map</option>
-    </select>
+    <div>
+      <select
+        onChange={(e) =>
+          setSelectedLayer(
+            baseLayerOptions.find((l) => l.id === e.target.value)!,
+          )
+        }
+        value={selectedLayer.id}
+      >
+        {baseLayerOptions.map(({ id, name }) => (
+          <option value={id}>{name}</option>
+        ))}
+      </select>
+      {selectedLayer.name}
+    </div>
   );
 }
