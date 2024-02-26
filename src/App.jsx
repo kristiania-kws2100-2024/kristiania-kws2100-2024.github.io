9fccc13d0b51697260d1cb3ffbe362223251824c
Johannes Brodwall <johannes@brodwall.com>
2024-02-27 00:31:43 +0100

    nine squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return <>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+  </>;
 }
