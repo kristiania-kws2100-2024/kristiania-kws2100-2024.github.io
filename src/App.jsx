0a6fc0f21a6f2dc35685e826982749d2fac7a93d
Johannes Brodwall <johannes@brodwall.com>
2024-02-27 00:45:34 +0100

    calculateWinner

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(index) {
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = [...squares];
     nextSquares[index] = xIsNext ? "X" : "0";
     setSquares(nextSquares);
@@ -38,4 +41,25 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   </>;
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
