13110e5b2346943eab51a7fbf39e7ba7e61ab0e0
Johannes Brodwall <johannes@brodwall.com>
2024-02-27 00:33:05 +0100

    group components into rows

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 export default function Square() {
   return <>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
+    <div className="board-row">
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
+    </div>
+    <div className="board-row">
+      <button className="square">4</button>
+      <button className="square">5</button>
+      <button className="square">6</button>
+    </div>
+    <div className="board-row">
+      <button className="square">7</button>
+      <button className="square">8</button>
+      <button className="square">9</button>
+    </div>
   </>;
 }
