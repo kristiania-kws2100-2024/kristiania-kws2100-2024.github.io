970ad65f967a66961957e6398f99e13b36506aa3
Johannes Brodwall <johannes@brodwall.com>
2024-02-27 01:12:37 +0100

    convert all to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App.tsx";
+import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
 
