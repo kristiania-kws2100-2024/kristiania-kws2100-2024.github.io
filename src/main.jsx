9d8a876ab69ba8ded6da7f712758447b48181d99
Johannes Brodwall <johannes@brodwall.com>
2024-02-27 01:06:36 +0100

    JavaScript is valid TypeScript

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import App from "./App.tsx";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
