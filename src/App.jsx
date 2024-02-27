4b9ea0150dcce1ce0cdb0ec358cd870b78f63522
Johannes Brodwall <johannes@brodwall.com>
2024-02-27 01:00:25 +0100

    implementing time travel

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,19 @@ function Square({value, onSquareClick}) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length-1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((square, move) => {
