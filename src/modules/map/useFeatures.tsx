d23be7805eee63ae038e2a862fd53840b11e25df
Johannes Brodwall <johannes@brodwall.com>
2024-02-28 21:12:31 +0100

    add jsdoc for useFeatures

--- src/modules/map/useFeatures.tsx
+++ src/modules/map/useFeatures.tsx
@@ -5,9 +5,50 @@ import { MapContext } from "./mapContext";
 import { useViewExtent } from "./useViewExtent";
 import VectorLayer from "ol/layer/Vector";
 
+interface UseFeaturesReturnType<T extends Feature> {
+  /**
+   * a filtered list of features which are currently shown on the view.
+   */
+  visibleFeatures: T[];
+  /**
+   * all features in the layer. Empty if the layerPredicate
+   * matched no currently displayed layers
+   */
+  features: T[];
+  /**
+   * active feature. When the user moves the pointer over the map,
+   * this will be updated to the feature under the pointer (if any)
+   */
+  activeFeature?: T;
+  /**
+   * Updates activeFeature manually. activeFeature will keep this value
+   * until the user moves the pointer over the map
+   */
+  setActiveFeature(feature?: T): void;
+}
+
+/**
+ * Uses layerPredicate to find a layer and returns the features of that
+ * layer. The return value contains all the features of the layer, as well
+ * as all visible features.
+ *
+ * You can specify the type of the Features returned which will let you
+ * have typesafe properties
+ *
+ * @param layerPredicate a function that returns a boolean for each layer.
+ * You should pass in a function that returns true for the layer you want
+ * to retrieve features for. For example
+ * ```(layer) => layer.getClassName() === "kommuner"```
+ * @return features, visibleFeatures, activeFeature and setActiveFeature
+ *
+ * @example
+ * const { visibleFeatures } = useFeatures<KommuneFeature>(
+ *  l => l.getClassName() === "kommuner"
+ * );
+ */
 export function useFeatures<T extends Feature>(
   layerPredicate: (layer: Layer) => boolean,
-) {
+): UseFeaturesReturnType<T> {
   const { layers, map } = useContext(MapContext);
   const viewExtent = useViewExtent();
   const layer = useMemo(
@@ -20,10 +61,12 @@ export function useFeatures<T extends Feature>(
     [features, viewExtent],
   );
   const [activeFeature, setActiveFeature] = useState<T>();
+
   function handlePointermove(e: MapBrowserEvent<MouseEvent>) {
     const features = layer?.getSource().getFeaturesAtCoordinate(e.coordinate);
     setActiveFeature(features?.length === 1 ? features[0] : undefined);
   }
+
   useEffect(() => {
     if (layer) {
       map.on("pointermove", handlePointermove);
