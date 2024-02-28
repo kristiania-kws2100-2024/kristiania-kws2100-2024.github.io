c126c0200fa615830f1a52e67229ce1dc14e7d94
Johannes Brodwall <johannes@brodwall.com>
2024-02-28 21:20:28 +0100

    add jsdoc for useVectorFeatures

--- src/modules/map/useVectorFeatures.tsx
+++ src/modules/map/useVectorFeatures.tsx
@@ -5,9 +5,39 @@ import { MapContext } from "./mapContext";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 
+/**
+ * Uses layerPredicate to find a layer and returns the features of that
+ * layer. The return value contains all the features of the layer, as well
+ * as all visible features.
+ *
+ * You can specify the type of the Features returned which will let you
+ * have typesafe properties
+ *
+ * @param layerSelector a function that returns a boolean for each layer.
+ * You should pass in a function that returns true for the layer you want
+ * to retrieve features for. For example
+ * ```(layer) => layer.getClassName() === "kommuner"```
+ * @return layer, features and visibleFeatures
+ *
+ * @example
+ * const { visibleFeatures } = useVectorFeatures<KommuneFeature>(
+ *  l => l.getClassName() === "kommuner"
+ * );
+ */
 export function useVectorFeatures<FEATURE extends Feature>(
   layerSelector: (l: Layer) => boolean,
-) {
+): {
+  layer?: Layer;
+  /**
+   * a filtered list of features which are currently shown on the view.
+   */
+  visibleFeatures: FEATURE[];
+  /**
+   * all features in the layer. Empty if the layerSelector
+   * matched no currently displayed layers
+   */
+  features: FEATURE[];
+} {
   const { map, layers } = useContext(MapContext);
   const layer = useMemo(
     () => layers.find(layerSelector),
