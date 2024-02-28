8d6869c26f4e3ab17d02ec719db21966747109e4
Johannes Brodwall <johannes@brodwall.com>
2024-02-28 22:55:39 +0100

    download kommuner_komprimert.json from https://www.eriksmistad.no/norges-fylker-og-kommuner-i-geojson-format/

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use((req, res, next) => {
 
 app.get("/api/profile", (req, res) => {
   res.json({ username: "Johannes" });
+});
+app.get("/api/kommuner", (req, res) => {
+  res.sendFile(path.resolve("./public/kommuner_komprimert.json"));
 });
 
 app.listen(3000);
