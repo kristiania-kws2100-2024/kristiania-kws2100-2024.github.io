bbcca888c587e4e7300ec30a8f1c5c0ed0892a1e
Johannes Brodwall <johannes@brodwall.com>
2024-03-06 10:12:35 +0100

    serve kommuner from database

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import pg from "pg";
-import * as path from "path";
 
 const postgresql = new pg.Pool({
   user: "postgres",
@@ -9,26 +8,18 @@ const postgresql = new pg.Pool({
 
 const app = express();
 
-app.get("/api/kommuner.new", async (req, res) => {
+app.get("/api/kommuner", async (req, res) => {
   const result = await postgresql.query(
-    "select kommunenummer, kommunenavn, omrade::json from kommuner",
+    "select kommunenummer, kommunenavn, st_transform(st_simplify(omrade, 10), 4326)::json as omrade from kommuner",
   );
-  console.log(result.rows[0].omrade);
   res.json({
     type: "FeatureCollection",
-    features: result.rows.map((row) => ({
+    features: result.rows.map(({ kommunenavn, kommunenummer, omrade }) => ({
       type: "Feature",
-      //geometry: row.omrade,
-      properties: {
-        kommunenummer: row.kommunenummer,
-        navn: row.kommunenavn,
-      },
+      geometry: omrade,
+      properties: { kommunenummer, kommunenavn },
     })),
   });
-});
-
-app.get("/api/kommuner", async (req, res) => {
-  res.sendFile(path.resolve("../public/kommuner.json"));
 });
 
 app.listen(3000);
