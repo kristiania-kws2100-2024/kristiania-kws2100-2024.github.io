06075da8bf8c068527b9ab131077304ee94f5f4d
Johannes Brodwall <johannes@brodwall.com>
2024-03-06 13:35:59 +0100

    backend is now reading kommuner from database

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,25 @@
 import express from "express";
-import * as path from "path";
+import pg from "pg";
+
+const postgresql = new pg.Pool({
+  user: "postgres",
+  database: "norway_data",
+});
 
 const app = express();
 
-app.get("/api/kommuner", (req, res) => {
-  res.sendFile(path.resolve("../public/kommuner.json"));
+app.get("/api/kommuner", async (req, res) => {
+  const result = await postgresql.query(
+    "select kommunenummer, kommunenavn, st_simplify(st_transform(omrade, 4326), 0.001)::json as geometry from kommuner",
+  );
+  res.json({
+    type: "FeatureCollection",
+    features: result.rows.map(({ kommunenummer, kommunenavn, geometry }) => ({
+      type: "Feature",
+      geometry,
+      properties: { kommunenummer, kommunenavn },
+    })),
+  });
 });
 
 app.listen(3000);
