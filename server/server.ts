3d7a00e5b8062320e0e4f25cf115dc52d2bd81e5
Johannes Brodwall <johannes@brodwall.com>
2024-03-06 10:36:05 +0100

    fix (api): explore grunnkrets in database (transform geometries on import)

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.get("/api/kommuner", async (req, res) => {
   const result = await postgresql.query(
-    "select kommunenummer, kommunenavn, st_transform(st_simplify(omrade, 10), 4326)::json as omrade from kommuner",
+    "select kommunenummer, kommunenavn, st_simplify(omrade, 0.0001)::json as omrade from kommuner",
   );
   res.json({
     type: "FeatureCollection",
