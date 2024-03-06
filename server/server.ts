a1afede9bb89d4a25b4ef4099334fdb6fd74b356
Johannes Brodwall <johannes@brodwall.com>
2024-03-06 09:56:24 +0100

    serve kommuner from express

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello world");
+import express from "express";
+import * as path from "path";
+
+const app = express();
+
+app.get("/api/kommuner", (req, res) => {
+  res.sendFile(path.resolve("../public/kommuner.json"));
+});
+
+app.listen(3000);
