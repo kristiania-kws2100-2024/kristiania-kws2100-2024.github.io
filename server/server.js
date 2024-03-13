7f7790b2bfd115b593c4f9a9ce4323e6581a9341
Johannes Brodwall <johannes@brodwall.com>
2024-03-13 13:35:40 +0100

    very basic API

--- server/server.js
+++ server/server.js
@@ -2,5 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/api/grunnkretser", (req, res) => {
+  res.json({
+    hello: "flat earth"
+  })
+})
+
 app.listen(3000);
 
