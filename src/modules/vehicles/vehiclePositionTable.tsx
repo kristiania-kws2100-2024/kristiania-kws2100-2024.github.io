278bb2b021551aa2b072d3fa5d420eb7edc717ee
Johannes Brodwall <johannes@brodwall.com>
2024-04-01 00:02:52 +0200

    Extract vehicleRow

--- src/modules/vehicles/vehiclePositionTable.tsx
+++ src/modules/vehicles/vehiclePositionTable.tsx
@@ -3,6 +3,20 @@ import { useVehiclePositions } from "./vehiclePositionsContext";
 import { Time } from "../../components/time";
 import { Coordinates } from "../../components/coordinates";
 import { sortBy } from "../../lib/sortBy";
+import { FeedEntity } from "../../../generated/gtfs-realtime";
+
+function VehicleRow(props: { vehicle: FeedEntity }) {
+  const v = props.vehicle;
+  const { vehicle } = v;
+  if (!vehicle) return null;
+  return (
+    <div>
+      <Time epocSeconds={vehicle.timestamp} />{" "}
+      <Coordinates latitudeLongitude={vehicle.position} />{" "}
+      {vehicle.trip?.routeId}
+    </div>
+  );
+}
 
 export function VehiclePositionTable() {
   const { lastSnapshot } = useVehiclePositions();
@@ -14,11 +28,7 @@ export function VehiclePositionTable() {
     <>
       <h1>{vehicles.length} vehicles</h1>
       {vehicles.sort(sortBy((p) => -p.vehicle?.timestamp!)).map((v) => (
-        <div key={v.id}>
-          <Time epocSeconds={v.vehicle?.timestamp} />{" "}
-          <Coordinates latitudeLongitude={v.vehicle?.position} />{" "}
-          {v.vehicle?.trip?.routeId}
-        </div>
+        <VehicleRow key={v.id} vehicle={v} />
       ))}
     </>
   );
