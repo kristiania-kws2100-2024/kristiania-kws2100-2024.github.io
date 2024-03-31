08ccdd03ecf6cfbe199a4cd64576708333d385c8
Johannes Brodwall <johannes@brodwall.com>
2024-03-31 13:29:31 +0200

    Display vehicle status in table

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { useVehicles } from "./vehicles/useVehicles";
+import { Application } from "./vehicles/application";
 
 const root = createRoot(document.getElementById("root")!);
-
-function Application() {
-  const vehicles = useVehicles();
-  return <h1>Hello world, there are {vehicles.length} positions</h1>;
-}
 
 root.render(<Application />);
