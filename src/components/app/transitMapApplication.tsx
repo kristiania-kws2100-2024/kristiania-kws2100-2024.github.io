f6cfee3a735a5f13026574935c006d95a130536a
Johannes Brodwall <johannes@brodwall.com>
2024-04-03 13:37:16 +0200

    Refactor vehicle parsing code

--- src/components/app/transitMapApplication.tsx
+++ src/components/app/transitMapApplication.tsx
@@ -1,26 +1,62 @@
 import { Feature, Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
-import React, { MutableRefObject, useEffect, useRef } from "react";
+import React, {
+  MutableRefObject,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
-import { FeedMessage } from "../../../generated/gtfs-realtime";
+import { FeedMessage, VehiclePosition } from "../../../generated/gtfs-realtime";
 import { Point } from "ol/geom";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 
 useGeographic();
 
-const vehicleSource = new VectorSource();
-const vehicleLayer = new VectorLayer({ source: vehicleSource });
-
+const backgroundLayer = new TileLayer({ source: new OSM() });
 const map = new Map({
   view: new View({ center: [10, 63], zoom: 8 }),
-  layers: [new TileLayer({ source: new OSM() }), vehicleLayer],
 });
 
+interface KwsVehicle {
+  routeId: string;
+  coordinate: number[];
+}
+
+function convertFromProtobuf(
+  vehicle: VehiclePosition | undefined,
+): KwsVehicle | undefined {
+  if (!vehicle) return;
+  const { position, trip } = vehicle;
+  if (!position || !trip) return;
+  const { latitude, longitude } = position;
+  const { routeId } = trip;
+  if (!routeId) return;
+
+  return {
+    routeId,
+    coordinate: [longitude, latitude],
+  };
+}
+
 export function TransitMapApplication() {
+  const [vehicleSource, setVehicleSource] = useState<VectorSource>();
+  const vehicleLayer = useMemo(
+    () => new VectorLayer({ source: vehicleSource }),
+    [vehicleSource],
+  );
+  const layers = useMemo(() => {
+    return [backgroundLayer, vehicleLayer];
+  }, [vehicleLayer]);
+  useEffect(() => {
+    map.setLayers(layers);
+  }, [layers]);
+
   async function fetchVehiclePosition() {
     const res = await fetch(
       "https://api.entur.io/realtime/v1/gtfs-rt/vehicle-positions",
@@ -31,16 +67,16 @@ export function TransitMapApplication() {
     const responseMessage = FeedMessage.decode(
       new Uint8Array(await res.arrayBuffer()),
     );
-    console.log(responseMessage);
-
+    const vehicles: KwsVehicle[] = [];
     for (const { vehicle } of responseMessage.entity) {
-      if (!vehicle) continue;
-      const { position } = vehicle;
-      if (!position) continue;
-      const { latitude, longitude } = position;
-      const point = new Point([longitude, latitude]);
-      vehicleSource.addFeature(new Feature(point));
+      const v = convertFromProtobuf(vehicle);
+      if (v) vehicles.push(v);
     }
+    setVehicleSource(
+      new VectorSource({
+        features: vehicles.map((v) => new Feature(new Point(v.coordinate))),
+      }),
+    );
   }
 
   useEffect(() => {
