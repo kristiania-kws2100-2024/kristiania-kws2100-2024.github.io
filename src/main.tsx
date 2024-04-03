116cfbca23c28db19d796f6d1a77c4312b1b9578
Johannes Brodwall <johannes@brodwall.com>
2024-04-03 12:31:50 +0200

    Added checking with husky for tsc and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root")!)
-root.render(<h1>Hello world</h1>)
+const root = createRoot(document.getElementById("root")!);
+root.render(<h1>Hello world</h1>);
