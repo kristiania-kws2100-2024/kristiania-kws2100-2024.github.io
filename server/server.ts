2b852bf422a9e1fe9ef7d4ec742c59a379c5bc86
Johannes Brodwall <johannes@brodwall.com>
2024-05-06 23:18:05 +0200

    Run database and expose with express

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import pg from "pg";
+
+const postgresql = new pg.Pool({ user: "postgres" });
 
 const app = express();
 
 app.get("/api/hello", (req, res) => {
   res.json({ hello: "world" });
+});
+
+app.get("/api/srids", async (req, res) => {
+  const result = await postgresql.query("select * from spatial_ref_sys");
+  res.json(result.rows.map(({ srid, auth_name }) => ({ srid, auth_name })));
 });
 
 app.listen(3000);
