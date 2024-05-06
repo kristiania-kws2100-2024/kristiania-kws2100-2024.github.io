6ffad30f4d3486e3f169547ee398d2fc3196476f
Johannes Brodwall <johannes@brodwall.com>
2024-05-07 00:11:43 +0200

    2. Display features

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { MutableRefObject, useEffect, useRef } from "react";
+import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
 
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
@@ -9,17 +9,42 @@ import { useGeographic } from "ol/proj";
 import "ol/ol.css";
 
 import "./application.css";
+import { Layer } from "ol/layer";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
 
 useGeographic();
 
+const osmLayer = new TileLayer({ source: new OSM() });
+const shelterLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/shelters.json",
+    format: new GeoJSON(),
+  }),
+});
+
 const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
   view: new View({ center: [10, 60], zoom: 9 }),
 });
 
 export function Application() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
   useEffect(() => map.setTarget(mapRef.current), []);
+
+  const [featureLayers, setFeatureLayers] = useState<Layer[]>([]);
+
+  const layers = useMemo(() => [osmLayer, ...featureLayers], [featureLayers]);
+  useEffect(() => map.setLayers(layers), [layers]);
+
+  const [showShelters, setShowShelters] = useState(false);
+  useEffect(() => {
+    if (showShelters) {
+      setFeatureLayers((old) => [...old, shelterLayer]);
+    } else {
+      setFeatureLayers((old) => old.filter((l) => l !== shelterLayer));
+    }
+  }, [showShelters]);
 
   return (
     <>
@@ -28,7 +53,11 @@ export function Application() {
       </header>
       <nav>
         <label>
-          <input type={"checkbox"} />
+          <input
+            type={"checkbox"}
+            checked={showShelters}
+            onChange={(e) => setShowShelters(e.target.checked)}
+          />
           Show shelters
         </label>
         <label>
